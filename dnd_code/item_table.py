1ae35a942a3a1545326466e516202c8b6dd95a8f
Are Sløtte <aretov@live.com>
2021-07-07 16:57:10 +0200

    Pre testing of finished individual treasure generating

--- dnd_code/item_table.py
+++ dnd_code/item_table.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 table = pd.read_CSV("Loot_Chance.csv")
 
-def Individual_loot(APL, Type, Setting = 3, CR):
+def Individual_loot(APL, Type, Setting = "High", CR):
     #Different chances based on challenge rating
     if CR <= 8:
         new_Table = table.drop(columns = ["CR_10+", "CR_9"])
@@ -12,7 +12,16 @@ def Individual_loot(APL, Type, Setting = 3, CR):
     elif 10 <= CR:
         new_Table = table.drop(columns = ["CR_0_to_8", "CR_9"])
 
+    Setting = Setting.lower()
+
+    if Setting == "high":
+        mul = 3
+    elif Setting == "mid":
+        mul = 2
+    elif Setting == "low":
+        mul = 1
     
+
     #decider for if there is loot or not rand <= new_Table CR value gives loot
     rand = np.random.uniform()
     #Gets the targets chance
@@ -20,6 +29,46 @@ def Individual_loot(APL, Type, Setting = 3, CR):
     name = target.columns[1]    #Gets the column name of the second column to get the value
     val = target[[name]].values[0][0]   #Must be a better way than this but for now it works
 
+    if rand <= val:
+        if Type == "Aberration":
+            return d12(2) * mul
+        elif Type == "Beast":
+            return 1 * min1(mul - 1)
+        elif Type == "Celestial":
+            return d10(2) * APL * min1(mul - 1)
+        elif Type == "Construct":
+            return 1 * min1(mul - 1)
+        elif Type == "Dragon":
+            return 0
+        elif Type == "Elemental":
+            return 0
+        elif Type == "Fey":
+            return d4(1) * APL * mul
+        elif Type == "Fiend":
+            return d8(2) * APL * mul
+        elif Type == "Giant":
+            return d4(2) * APL * mul
+        elif Type == "Humanoid":
+            return d6(2) * APL * min1(mul - 1)  #Need a fix for low setting to be 0.5 instead of 1. Not priority atm
+        elif Type == "Monstrosity":
+            return d4(1) * (mul - 1)
+        elif Type == "Ooze":
+            return d12(2) * APL * min1(mul - 1)
+        elif Type == "Plant":
+            return d10(2) * min1(mul - 1)
+        elif Type == "Undead":
+            return d4(2) * APL * min1(mul - 1)
+
+
+        
+    else:
+        return 0
+
+def min1(x):
+    if x <=1:
+        return 1
+    else:
+        return x:
 
 def d12(x):
     s = 0
