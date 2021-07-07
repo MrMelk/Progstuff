fa94b1b750a565f64cc66b02d7ec68c545296e86
Are Sløtte <aretov@live.com>
2021-07-07 17:27:49 +0200

    Finished, tested, working individual treasure generating

--- dnd_code/item_table.py
+++ dnd_code/item_table.py
@@ -1,9 +1,9 @@
 import numpy as np
 import pandas as pd
 
-table = pd.read_CSV("Loot_Chance.csv")
+table = pd.read_csv("Loot_Chance.csv")
 
-def Individual_loot(APL, Type, Setting = "High", CR):
+def Individual_loot(APL, Type, CR, Setting = "High"):
     #Different chances based on challenge rating
     if CR <= 8:
         new_Table = table.drop(columns = ["CR_10+", "CR_9"])
@@ -64,66 +64,44 @@ def Individual_loot(APL, Type, Setting = "High", CR):
     else:
         return 0
 
+#Does not allow a value to be smaller than 1.
 def min1(x):
     if x <=1:
         return 1
     else:
-        return x:
+        return x
 
+#12 sided die
 def d12(x):
     s = 0
-    for i in range x:
+    for i in range(x):
         s += np.random.randint(1, 12)
     return s
 
+#Ten sided die
 def d10(x):
     s = 0
-    for i in range x:
+    for i in range(x):
         s += np.random.randint(1, 10)
     return s
 
+#Eight sided die
 def d8(x):
     s = 0
-    for i in range x:
+    for i in range(x):
         s += np.random.randint(1, 8)
     return s
 
+#Six sided die
 def d6(x):
     s = 0
-    for i in range x:
+    for i in range(x):
         s += np.random.randint(1, 6)
     return s
 
+#Four sided die
 def d4(x):
     s = 0
-    for i in range x:
+    for i in range(x):
         s += np.random.randint(1, 4)
     return s
-"""
-#Tossing current idea to try for more orderly solution, will return if needed
-
-
-Monster_type = ["Abberation", "Beast", "Celestial", "Construct", "Dragon", "Elemental", 
-"Fey", "Fiend", "Giant", "Humanoid", "Monstrosity", "Ooze", "Plant", "Undead"]
-
-
-
-
-
-
-def Individual_Treasure(APL, type, setting, CR):
-    if 0 <= CR >= 8:
-        Treasure_chance = {"Abberation": 0.01, "Beast": 0.01, "Celestial": 0, "Construct":0.0, "Dragon": 0, "Elemental": 0,
-        "Fey": 0.8,  "Fiend": 0.9, "Giant": 0.95, "Humanoid":0.95, "Monstrosity": 0.1, "Ooze": 0.8, "Plant":0.4, "Undead":0.2}
-    elif CR == 9:
-        Treasure_chance = {"Abberation": 0.4, "Beast": 0.01, "Celestial": 0, "Construct":0.0, "Dragon": 0, "Elemental": 0,
-        "Fey": 0.8,  "Fiend": 0.9, "Giant": 0.95, "Humanoid":0.95, "Monstrosity": 0.1, "Ooze": 0.8, "Plant":0.4, "Undead":0.2}
-    elif CR >= 10:
-        Treasure_chance = {"Abberation": 0.4, "Beast": 0.01, "Celestial": 0.9, "Construct":0.0, "Dragon": 0, "Elemental": 0,
-        "Fey": 0.8,  "Fiend": 0.9, "Giant": 0.95, "Humanoid":0.95, "Monstrosity": 0.1, "Ooze": 0.8, "Plant":0.4, "Undead":0.2}
-
-    rand = np.random.uniform()
-    Treasure_Amount = {"Abberation": np.random.randint(1,12)+ np.random.randint(1,12),
-    "Beast": 1, "Celestial"}
-
-"""
