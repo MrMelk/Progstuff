84ff1456f7f84365487a9ae6680b8dd7282d272b
Are Sløtte <aretov@live.com>
2021-07-07 13:44:59 +0200

    adding dice functions + small fixes

--- dnd_code/item_table.py
+++ dnd_code/item_table.py
@@ -6,22 +6,50 @@ table = pd.read_CSV("Loot_Chance.csv")
 def Individual_loot(APL, Type, Setting = 3, CR):
     #Different chances based on challenge rating
     if CR <= 8:
-        chance = table[["CR_10+", "CR_9"]]
+        new_Table = table.drop(columns = ["CR_10+", "CR_9"])
     elif CR == 9:
-        chance = table.drop[["CR_0_to_8", "CR_10+"]]
+        new_Table = table.drop(columns = ["CR_0_to_8", "CR_10+"])
     elif 10 <= CR:
-        chance = table[["CR_0_to_8", "CR_9"]]
+        new_Table = table.drop(columns = ["CR_0_to_8", "CR_9"])
 
     
-    #decider for if there is loot or not rand <= chance gives loot
+    #decider for if there is loot or not rand <= new_Table CR value gives loot
     rand = np.random.uniform()
     #Gets the targets chance
-    target = chance.loc[chance["Type"] == Type]
+    target = new_Table.loc[new_Table["Type"] == Type]
     name = target.columns[1]    #Gets the column name of the second column to get the value
     val = target[[name]].values[0][0]   #Must be a better way than this but for now it works
 
 
+def d12(x):
+    s = 0
+    for i in range x:
+        s += np.random.randint(1, 12)
+    return s
 
+def d10(x):
+    s = 0
+    for i in range x:
+        s += np.random.randint(1, 10)
+    return s
+
+def d8(x):
+    s = 0
+    for i in range x:
+        s += np.random.randint(1, 8)
+    return s
+
+def d6(x):
+    s = 0
+    for i in range x:
+        s += np.random.randint(1, 6)
+    return s
+
+def d4(x):
+    s = 0
+    for i in range x:
+        s += np.random.randint(1, 4)
+    return s
 """
 #Tossing current idea to try for more orderly solution, will return if needed
 
